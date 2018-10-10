8dc165a875b0c8af22a243f8f8880cf7b86da4be
Barsukov Nikita <Barsik98n@mail.ru>
2018-10-10 20:48:36 +0300

    Добавлены основные прототипы структур

---
+++ Lab_02/library.h
@@ -0,0 +1,17 @@
+#ifndef LIBRARY_H
+#define LIBRARY_H
+
+namespace library{
+    struct Library;
+    struct Book;
+    struct TechBook;
+    struct ArtBook;
+    struct ChildBook;
+    union BookType;
+    enum bookTypeKey;
+    enum TechBookType;
+    enum ArtBookType;
+    enum ChildBookType;
+}
+
+#endif // LIBRARY_H
