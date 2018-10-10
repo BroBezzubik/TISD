668752538bd588f2017e8dfdfc10c2d52e784f4b
Barsukov Nikita <Barsik98n@mail.ru>
2018-10-10 21:04:02 +0300

    Добавлена реализация структуры Book, Library

--- Lab_02/library.h
+++ Lab_02/library.h
@@ -1,6 +1,9 @@
 #ifndef LIBRARY_H
 #define LIBRARY_H
 
+#define TABLE_SIZE 1000
+#define BUFFER_SIZE 50
+
 namespace library{
     struct Library;
     struct Book;
@@ -12,6 +15,20 @@ namespace library{
     enum TechBookType;
     enum ArtBookType;
     enum ChildBookType;
+}
+
+struct library::Library{
+    char fileName[BUFFER_SIZE];
+    Book table[TABLE_SIZE];
+    int bookCout;
+}
+
+struct library::Book{
+    char autorName[BUFFER_SIZE];
+    char bookName[BUFFER_SIZE];
+    char publisher[BUFFER_SIZE];
+    int pageCout;
+    BookType type;
 }
 
 #endif // LIBRARY_H
